0669acb3a0d7cf4c7e300d3fb1d871b091c3820f
Guillaume Maiano <guillaume@maiano.fr>
2021-06-08 11:26:09 +0200

    Naive implementation using channels

--- routines/linkChecks.go
+++ routines/linkChecks.go
@@ -4,18 +4,22 @@ import "log"
 import "net/http"
 
 func checkLinks( urls []string) {
+	c := make(chan string)
 	for _, url := range urls {
-		checkLink(url)
+		go checkLink(url, c)
+		log.Println(<- c)
 	}
 }
 
-func checkLink(url string) {
+func checkLink(url string, c chan string) {
 	log.Printf("Checking %s\n", url)
 	httpsUrl := "https://" + url
 	_, err := http.Get(httpsUrl)
 	if err != nil {
 		log.Println(httpsUrl, "could not be reached.")
+		c <- "failed"
 	} else {
 		log.Println(httpsUrl, "is online.")
+		c <- "success"
 	}
 }
