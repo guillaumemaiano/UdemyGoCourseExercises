898e3c72e3ffcd8e280843dc7d81565405455061
Guillaume Maiano <guillaume@maiano.fr>
2021-06-08 13:26:36 +0200

    Improved version using channels

--- routines/linkChecks.go
+++ routines/linkChecks.go
@@ -7,8 +7,15 @@ func checkLinks( urls []string) {
 	c := make(chan string)
 	for _, url := range urls {
 		go checkLink(url, c)
-		// this is a blocking statement and it makes the previous line kinda pointless
-		log.Println(<- c)
+	}
+	reportFails := 0
+	for i:= 0; i < len(urls); i++ {
+		if ("success" != <-c) {
+			reportFails += 1
+		}
+	}
+	if reportFails > 0 {
+		log.Printf("Encountered %d failures.\n", reportFails)
 	}
 }
 
